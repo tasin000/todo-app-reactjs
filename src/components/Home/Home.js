edc752c4e51eb0235de0c8e316d0165d68f26524
Md. Tasin Anwar <91390498+tasin000@users.noreply.github.com>
2022-05-02 22:20:38 +0600

    Intial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './Home.css';
+
+const Home = () => {
+     return (
+          <div>
+               <header>
+                    <Header />
+               </header>
+          </div>
+     );
+};
+
+export default Home;
