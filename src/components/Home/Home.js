0c16394379c20eee3270aa6f69b0b9dca4feccbf
Md. Tasin Anwar <91390498+tasin000@users.noreply.github.com>
2022-05-02 22:50:46 +0600

    Added Header and TodoInfo Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from '../Header/Header';
+import TodoInfo from '../TodoInfo/TodoInfo';
 import './Home.css';
 
 const Home = () => {
      return (
-          <div>
+          <div className='home'>
                <header>
                     <Header />
                </header>
+
+               <main>
+                    <section id="todo-info">
+                         <TodoInfo />
+                    </section>
+               </main>
           </div>
      );
 };
---
+++ src/components/TodoInfo/TodoInfo.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import './TodoInfo.css';
+
+const TodoInfo = () => {
+     return (
+          <div className='form-container'>
+               <div className="input-group">
+                    <div className="form-control">
+                         <label htmlFor="title">Title: </label>
+                         <input type="text" name="title" id="title" />
+                    </div>
+
+                    <div className="form-control">
+                         <label htmlFor="desc">Description: </label>
+                         <textarea name="desc" id="desc" cols="30" rows="3"></textarea>
+                    </div>
+               </div>
+
+               <button type="submit">
+                    Add Todo
+                    <FontAwesomeIcon icon={faPlusCircle} />
+               </button>
+          </div>
+     );
+};
+
+export default TodoInfo;
